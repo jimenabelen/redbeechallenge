13f3565641247528efa937c2071b14ed4c2424ff
jimenabelen <67125175+jimenabelen@users.noreply.github.com>
2021-06-17 23:25:48 -0300

    Update CompareNumbers.java

--- src/redbeechallenges/CompareNumbers.java
+++ src/redbeechallenges/CompareNumbers.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package redbeechallenges;
 
-
+//Compare  three numbers.
 
 
 public class CompareNumbers {
